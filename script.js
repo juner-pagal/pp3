56b38af2e2c9b0375d554b35b88e7cefceaf6551
juner-pagal <juner.pagal@gmail.com>
2024-06-09 22:27:45 +0800

    Updating Navigation bar and footer of all pages using JavaScript

--- script.js
+++ script.js
@@ -31,6 +31,48 @@ navLinkEls.forEach(navLinkEl => {
 
     
 //Designing Footer
+let readyToGrow = document.getElementById("ready-to-grow");
+let readyToGrowContent = `
+      <div class="col"></div>
+        <div class="col-md-6">
+            <h3>Ready to grow</h3>
+            <h1>Get your free quote today</h1>
+                <form action="#">
+                    <div class="form-floating mb-3">
+                        <input type="url" class="form-control" id="web-url" placeholder="Web URL" required>
+                        <label for="web-url">Web URL</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="email" class="form-control" id="email" placeholder="Email" required>
+                        <label for="email">Email</label>
+                    </div>
+                    <div class="col-12">
+                        <button class="btn btn-primary" type="submit">Submit Now</button>
+                      </div>
+                </form>
+        </div>
+        <div class="col"></div>`;
 
+readyToGrow.innerHTML = readyToGrowContent;
+// readyToGrow.append(readyDivs);
 
-//---------------------------------
+//---------------------------------------------------------
+let myFooter = document.querySelector("footer");
+let footerDiv = document.createElement("div");
+footerDiv.classList.add("row");
+
+let footerContents = `
+              <div class="col-lg-4">
+              <p>Copyright &copy 2024</p>
+              </div>
+              <div class="col-lg-4">
+              <p>Metro Manila, Philippines</p>
+              </div>
+              <div class="col-lg-4">
+              <p>+639668338043</p>
+              </div>`;
+footerDiv.innerHTML = footerContents;
+
+myFooter.append(footerDiv);
+
+//---------End of Footer----------------
