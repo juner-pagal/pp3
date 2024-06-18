e21843175ceb157ebbb37bd8d79aab2bd69abd33
juner-pagal <juner.pagal@gmail.com>
2024-06-18 22:33:36 +0800

    modify pages, styles and script

--- script.js
+++ script.js
@@ -30,46 +30,43 @@ navLinkEls.forEach(navLinkEl => {
 });
 
 //Designing Review section
-const url = 'https://local-business-data.p.rapidapi.com/business-reviews?business_id=0x89c259b5a9bd152b%3A0x31453e62a3be9f76&limit=5&sort_by=most_relevant&region=us&language=en';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '417df4c886mshbe149c863baf4c7p18bfaajsna5a4dacc9762',
-		'x-rapidapi-host': 'local-business-data.p.rapidapi.com'
-	}
-};
+// const url = 'https://local-business-data.p.rapidapi.com/business-reviews?business_id=0x89c259b5a9bd152b%3A0x31453e62a3be9f76&limit=5&sort_by=most_relevant&region=us&language=en';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'x-rapidapi-key': '417df4c886mshbe149c863baf4c7p18bfaajsna5a4dacc9762',
+// 		'x-rapidapi-host': 'local-business-data.p.rapidapi.com'
+// 	}
+// };
 
 
-let list;
+// let list;
 
-let all  = document.getElementById('review');
-//asynch operation = promise
+// let all  = document.getElementById('review');
+// //asynch operation = promise
 
-fetch(url, options).then(response => response.json())
-.then(response => {
-    console.log(response.data);
-    list = response.data;
+// fetch(url, options).then(response => response.json())
+// .then(response => {
+//     console.log(response.data);
+//     list = response.data;
 
-// let row = document.createElement('div');
-// row.classList.add('row');
-let reviewRow = document.createElement('div');
-reviewRow.classList.add('row');
-// reviewRow.classList.add('align-items-center');
 
-    list.forEach((item) => {
-       let b = document.createElement('div');
-       b.classList.add('card');
-       b.innerHTML = `
-                <div class="card-body">
-                    <h5 class="card-title">${item.author_name}</h5>
-                    <p class="card-text card-text-justify"> ${item.review_text}</p>
-                </div>
-           `
-        //  all.append(container);
-         all.append(reviewRow);
-         reviewRow.appendChild(b);
-    })
-})
+// let reviewRow = document.createElement('div');
+// reviewRow.classList.add('row');
+
+//     list.forEach((item) => {
+//        let b = document.createElement('div');
+//        b.classList.add('card');
+//        b.innerHTML = `
+//                 <div class="card-body">
+//                     <h5 class="card-title">${item.author_name}</h5>
+//                     <p class="card-text card-text-justify"> ${item.review_text}</p>
+//                 </div>
+//            `
+//          all.append(reviewRow);
+//          reviewRow.appendChild(b);
+//     })
+// })
 
 //End of Review Section
     
@@ -100,22 +97,31 @@ readyToGrow.innerHTML = readyToGrowContent;
 // readyToGrow.append(readyDivs);
 
 //---------------------------------------------------------
-let myFooter = document.querySelector("footer");
-let footerDiv = document.createElement("div");
-footerDiv.classList.add("row");
+fetch('https://psgc.cloud/api/cities-municipalities')
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    // data.forEach(item => {
+    //   console.log(data[1064].name);
+    // })
+    let myFooter = document.querySelector("footer");
+      let footerDiv = document.createElement("div");
+      footerDiv.classList.add("row");
 
-let footerContents = `
-              <div class="col-lg-4">
-              <p>Copyright &copy 2024</p>
-              </div>
-              <div class="col-lg-4">
-              <p>Metro Manila, Philippines</p>
-              </div>
-              <div class="col-lg-4">
-              <p>+639668338043</p>
-              </div>`;
-footerDiv.innerHTML = footerContents;
+      let footerContents = `
+                    <div class="col-lg-4">
+                      <p>Copyright &copy 2024</p>
+                    </div>
+                    <div class="col-lg-4">
+                      <p>${data[1064].name}, Philippines</p>
+                    </div>
+                    <div class="col-lg-4">
+                      <p>+639668338043</p>
+                    </div>`;
+      footerDiv.innerHTML = footerContents;
 
-myFooter.append(footerDiv);
+      myFooter.append(footerDiv);
+  })
 
 //---------End of Footer----------------
